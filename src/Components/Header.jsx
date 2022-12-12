aa31dbd8128cfc3bfc6f4fcddda3e0c0b20c7bef
admin <admin@MacBook-Air-admin.local>
2022-12-12 13:08:35 +0100

    finished-firt-task

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/010/994/412/small/nike-logo-black-with-name-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320" />
+      </div>
+      <div>Avatar + description</div>
+      <div>
+        My posts
+        <div>New Posts</div>
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
